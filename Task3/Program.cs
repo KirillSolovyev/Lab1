c78268b86482cf0ef882f4fa007bbc9b03637b46
Kirill Solovyov <kirill.solovyev.98@gmail.com>
2019-01-27 15:24:55 +0600

    d

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3 {
+    class Program {
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = Array.ConvertAll<string, int>(Console.ReadLine().Split(), int.Parse);
+            for(int i = 0; i < arr.Length; i++) {
+                Console.Write(arr[i] + " " + arr[i] + " ");
+            }
+        }
+    }
+}
