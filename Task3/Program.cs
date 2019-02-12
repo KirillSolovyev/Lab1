f732637225b3b4f9339cf5307541e16b4c6dcf0c
Kirill Solovyov <kirill.solovyev.98@gmail.com>
2019-02-12 23:46:02 +0600

    Added comments

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -7,16 +7,16 @@ using System.Threading.Tasks;
 namespace Task3 {
     class Program {
         static void Main(string[] args) {
-            int n = Convert.ToInt32(Console.ReadLine());
-            int[] arr = Array.ConvertAll<string, int>(Console.ReadLine().Split(), int.Parse);
-            int[] SecArr = new int[n * 2];
-            int cnt = -1;
-            for(int i = 0; i < arr.Length; i++) {
+            int n = Convert.ToInt32(Console.ReadLine()); //Get length of the array 
+            int[] arr = Array.ConvertAll<string, int>(Console.ReadLine().Split(), int.Parse); // Get array
+            int[] SecArr = new int[n * 2]; // Create new array 
+            int cnt = -1; // counter
+            for(int i = 0; i < arr.Length; i++) { // Go through the initial array and inserting elements to the second array
                 SecArr[++cnt] = arr[i];
                 SecArr[++cnt] = arr[i];
             }
 
-            foreach(int el in SecArr) {
+            foreach(int el in SecArr) { // Print all 
                 Console.Write(el + " ");
             }
         }
