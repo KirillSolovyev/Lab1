61a40e8ce22770eef0f329c1504c448d873fd1ff
Kirill Solovyov <kirill.solovyev.98@gmail.com>
2019-02-12 23:38:01 +0600

    sdsdsdsdsdsi

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -9,8 +9,15 @@ namespace Task3 {
         static void Main(string[] args) {
             int n = Convert.ToInt32(Console.ReadLine());
             int[] arr = Array.ConvertAll<string, int>(Console.ReadLine().Split(), int.Parse);
+            int[] SecArr = new int[n * 2];
+            int cnt = -1;
             for(int i = 0; i < arr.Length; i++) {
-                Console.Write(arr[i] + " " + arr[i] + " ");
+                SecArr[++cnt] = arr[i];
+                SecArr[++cnt] = arr[i];
+            }
+
+            foreach(int el in SecArr) {
+                Console.Write(el + " ");
             }
         }
     }
